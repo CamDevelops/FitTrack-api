ffa0ae062376a029db5bf03536e9d1fe0a91157e
Cameron Mack <cameron@Camerons-MacBook-Air.local>
2026-03-21 13:10:55 -0400

    added password hashing using bcrypt

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request
+import bcrypt
 
 app = Flask(__name__)
 
 users = []
 required_fields = ["name", "age", "email", "password", "username", "number"]
-
 
 @app.route("/signup", methods=["POST"])
 def create_users():
@@ -18,9 +18,17 @@ def create_users():
     if missing_fields:
         return jsonify({"error": f"{missing_fields} missing"})
 
+    uh_password = new_user.get('password')
+    password_bytes = uh_password.encode("utf-8")
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    json_password = hashed_password.decode('utf-8')
+
+    new_user['password'] = json_password
+
     users.append(new_user)
     return jsonify(users)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
