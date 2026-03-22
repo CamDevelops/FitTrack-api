4c36ba3abd26304ebb1bcd77e510abf8eadf8aa6
Cameron Mack <cameron@Camerons-MacBook-Air.local>
2026-03-22 00:49:23 -0400

    refactor variable names for clarity and readability

--- routes/auth.py
+++ routes/auth.py
@@ -3,51 +3,51 @@ import bcrypt
 
 auth = Blueprint('auth', __name__)
 
-users = []
-required_fields = ["email", "username", "password", "name", "number", "age"]
+registered_users = []
+required_user_fields = ["email", "username", "password", "name", "number", "age"]
 
 @auth.route("/signup", methods=["POST"])
-def create_users():
-    new_user = request.json
+def signup():
+    user_data = request.json
 
     missing_fields = []
-    for field in required_fields:
-        if field not in new_user:
+    for field in required_user_fields:
+        if field not in user_data:
             missing_fields.append(field)
     if missing_fields:
         return jsonify({"error": f"{missing_fields} missing"})
 
-    user_email = new_user.get('email')
-    email_requirements = ['@', '.']
-    for char in email_requirements:
-        if char not in user_email:
+    email = user_data.get('email')
+    required_email_chars = ['@', '.']
+    for char in required_email_chars:
+        if char not in email:
             return jsonify({"error": "Please Enter a valid email."})
 
-    user_age = new_user.get('age')
-    if not isinstance(user_age, int):
+    age = user_data.get('age')
+    if not isinstance(age, int):
         return jsonify({"error": "Enter a valid Age."})
 
-    user_password = new_user.get('password')
-    if len(user_password) <= 11:
+    password = user_data.get('password')
+    if len(password) <= 11:
         return jsonify({"error": "Password must be at least 12 characters long."})
 
-    special_char = ['!', '@', '#', '$', '%', '^', '&', '*']
-    if not any(item in user_password for item in special_char):
+    required_special_chars = ['!', '@', '#', '$', '%', '^', '&', '*']
+    if not any(char in password for char in required_special_chars):
         return jsonify({"error": "Password must contain a special Character !@#$%^&*"})
 
-    if " " in user_password:
+    if " " in password:
         return jsonify({"error": "Password can't have spaces in it."})
 
     try:
-        password_bytes = user_password.encode("utf-8")
+        password_bytes = password.encode("utf-8")
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(password_bytes, salt)
-        json_password = hashed_password.decode('utf-8')
+        hashed_password_str = hashed_password.decode('utf-8')
 
-        new_user['password'] = json_password
+        user_data['password'] = hashed_password_str
 
-        users.append(new_user)
-        return jsonify(users)
+        registered_users.append(user_data)
+        return jsonify(registered_users)
 
     except ValueError:
         return jsonify({"error": "Invalid password format."})
